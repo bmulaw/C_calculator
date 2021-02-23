e945b5415912125c5b46949d2a050f6b245733fa
Bernard Mulaw <bmulaw@bu.edu>
2021-02-23 13:06:31 -0800

    Update math.h

--- math.h
+++ math.h
@@ -1,16 +1,11 @@
-//
-// Created by User on 2/13/2021.
-//
-
-#ifndef PROBLEMSETS_MATH_H
-#define PROBLEMSETS_MATH_H
+#ifndef PS_MATH_H
+#define PS_MATH_H
 
 float add(float, float);
 float subtract(float, float);
 float multiply(float, float);
-float divide(float, float);
+float divide(float, float) ;
 float power(float, float);
 float recursivePower(float, float);
 
-
-#endif //PROBLEMSETS_MATH_H
+#endif //PS_MATH_H
